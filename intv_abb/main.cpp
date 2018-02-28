1a4b744be4aecb2581fb69854cbd91061e2100e6
vanyail <vanyail@yandex.ru>
2018-02-28 17:42:45 +0300

    ы
    ы

---
+++ intv_abb/main.cpp
@@ -0,0 +1,51 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(){
+    int x = 0;
+    size_t N = 10;
+
+    std::cin>>x;
+
+    int *sorted_array = new int[N];
+    
+    sorted_array[0] = rand() % N;
+    std::cout<<sorted_array[0]<<" ";
+    
+    for (size_t i = 1; i < N; i++){
+        sorted_array[i] = sorted_array[i - 1] + (rand() % N);
+        std::cout<<sorted_array[i]<<" ";
+    }
+
+    std::cout<<'\n';
+
+    if (x <= sorted_array[0] || x >= 2 * sorted_array[N-1]){
+        std::cout<<"False"<<std::endl;
+        return 0;
+    }
+
+
+    for (size_t i = 0; i < N; i++){
+        for (size_t j = N; j > i; j--){
+
+            if (sorted_array[i] > x){
+                std::cout<<"False"<<std::endl;
+                return 0;
+            }
+
+            if (sorted_array[j] > x){
+                continue;
+            }
+
+            if ((sorted_array[i] + sorted_array[j]) == x){
+                std::cout<<"True"<<std::endl;
+                return 0;
+            }
+
+        }
+    }
+
+    delete [] sorted_array;
+    
+    return 0;
+}
